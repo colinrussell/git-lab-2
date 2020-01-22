5a01ae04c9793b015d0b07844252bfb1d21bfaa3
colinrussell <cr944418@ohio.edu>
2020-01-21 20:15:21 -0500

    1st submission of this program

---
+++ git-lab-program.cc
@@ -0,0 +1,21 @@
+/*
+ *
+ *      File: git-lab-program.cc
+ *      Author: Colin Russell
+ *      Date: Tuesday, January 21,2020
+ * Description: This program is developed in C++ code
+ *              for the Git lab. It is lab #2 for CS 2400.
+ * */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int arc, char const *argv[]){
+    //Colin Russell
+    // 1/21/2019
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
+
